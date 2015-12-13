f48f957e229a02fb66d69cab4384968866a7a11e
Bernat Martínez Vidal <bernatmv@gmail.com>
2015-12-13 16:39:09 +0100

    (feat) basic for app logic

--- Assets/Scripts/App/Logic/AppLogic.cs
+++ Assets/Scripts/App/Logic/AppLogic.cs
@@ -4,14 +4,24 @@ using com.lovelydog;
 
 public class AppLogic : FacadeMonoBehaviour 
 {
+    int currentQuestion = 0;
+
     void Awake()
     {
         _dispatcher.AddListener("start", StartQuiz);
+        _dispatcher.AddListener("next_question", StartQuiz);
     }
 
     void StartQuiz(Object param)
     {
         _dispatcher.Dispatch("show_question");
         _dispatcher.Dispatch("hide_main");
+        NextQuestion();
+    }
+
+    void NextQuestion()
+    {
+        currentQuestion++;
+        _dispatcher.Dispatch("next_question", new PayloadObject(currentQuestion));
     }
 }
